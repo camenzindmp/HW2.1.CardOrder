16d900f74c2ec214f2bce9bcc5367bb884d15f20
Максим Пенкин <m.penkin@gmail.com>
2020-09-28 09:51:37 +1000

    bad test added

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -21,7 +21,7 @@ public class TestClass {
     }
 
     @Test
-    void SuccessCase() {
+    void successCase() {
         driver.get("http://localhost:9999/");
         driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Дефолтное Имя");
         driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+71717171717");
@@ -29,5 +29,16 @@ public class TestClass {
         driver.findElement(By.cssSelector(".button__text")).submit();
         String successText = driver.findElement(By.cssSelector("[data-test-id]")).getText();
         assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", successText);
+    }
+
+    @Test
+    void firstDigitIsEight() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Дефолтное Имя");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("81717171717");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String wrongNumberText = driver.findElement(By.cssSelector("[data-test-id=phone] input")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", wrongNumberText);
     }
 }
