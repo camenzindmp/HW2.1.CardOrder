139a357ece710fb88df4e64eb87ad9b4186dbdc9
camenzindmp <camenzindmp@gmail.com>
2020-09-27 13:07:38 +1000

    Success test case done

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,14 +1,17 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClass {
     private WebDriver driver;
 
     @BeforeAll
-    static void first() {
+    static void driver() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
@@ -17,10 +20,14 @@ public class TestClass {
         driver = new ChromeDriver();
     }
 
-
-
     @Test
-    void SomeTest() {
+    void SuccessCase() {
         driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Дефолтное Имя");
+        driver.findElement(By.cssSelector("input[type='tel']")).sendKeys("+71717171717");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).submit();
+        String successText = driver.findElement(By.cssSelector("[data-test-id]")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", successText);
     }
 }
