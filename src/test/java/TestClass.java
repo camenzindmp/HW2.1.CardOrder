bb15f6d4d29ede4ef6912b608f0506eb128c2a3f
camenzindmp <camenzindmp@gmail.com>
2020-09-27 12:11:03 +1000

    build.gradle modified for selenium

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void first() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+
+
+    @Test
+    void SomeTest() {
+        driver.get("http://localhost:9999/");
+    }
+}
