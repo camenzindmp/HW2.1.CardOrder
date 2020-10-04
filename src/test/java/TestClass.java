1d507cf6edcefc235d497a7d14e9fc47d9666c54
camenzindmp <camenzindmp@gmail.com>
2020-10-04 18:06:35 +1000

    all done

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestClass {
 
     private WebDriver driver;
-//кца
+
     @BeforeAll
     static void driver() {
         WebDriverManager.chromedriver().setup();
